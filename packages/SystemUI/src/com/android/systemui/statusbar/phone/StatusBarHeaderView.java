403822cb4dabbe286b80b76c70d45ddf57ba6098
Martinusbe <martinusbe@gmail.com>
2016-08-25 10:51:11 +0200

    Revert "SystemUI: Fix FC on changing orientation"
    This reverts commit ee232e1a9495d2e6a0919b825c425d9ce6c023ac.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -410,11 +410,9 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mEmergencyCallsOnly.setVisibility(mExpanded && mShowEmergencyCallsOnly ? VISIBLE : GONE);
         mBatteryLevel.setForceShown(mExpanded && mShowBatteryTextExpanded);
         mBatteryLevel.setVisibility(View.VISIBLE);
-        View v = mSettingsContainer.findViewById(R.id.tuner_icon);
-        if (v != null) {
-            v.setVisibility(
-                TunerService.isTunerEnabled(mContext) ? View.VISIBLE : View.INVISIBLE);
-        }
+        mSettingsContainer.findViewById(R.id.tuner_icon).setVisibility(
+                TunerService.isTunerEnabled(mContext) ? View.INVISIBLE : View.INVISIBLE);
+        TunerService.setTunerEnabled(mContext, true);
     }
 
     private void updateSignalClusterDetachment() {
