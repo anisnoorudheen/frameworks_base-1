f7f8da3ae93e2e25e28da225e3312dab8c73e724
Alx31 <alex31.couderc@gmail.com>
2016-08-28 10:58:58 +0200

    pastie derp

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -427,8 +427,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW),
-                    false, this, UserHandle.USER_ALL);
-            update();    
+                    false, this, UserHandle.USER_ALL);  
         }
 
         @Override
