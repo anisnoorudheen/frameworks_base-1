9864ee5cafe6181df134ee031dfdccd324597528
Max Weninger <max.weninger@gmail.com>
2016-10-14 08:56:30 +0200

    Add OmniSwitch as option for default recents [1/2]
    Change-Id: I3af7a7e493e7f3aa7ebc62a8dcae13b4695d864a
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- core/java/com/android/internal/util/tipsy/TipsyUtils.java
+++ core/java/com/android/internal/util/tipsy/TipsyUtils.java
@@ -16,6 +16,7 @@
 
 package com.android.internal.util.tipsy;
 
+import android.content.Intent;
 import android.content.res.Resources;
 
 import java.util.Locale;
@@ -26,4 +27,33 @@ public class TipsyUtils {
        return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
                Locale.CHINESE.getLanguage());
     }
+
+    // Omni Switch Constants
+
+    /**
+     * Package name of the omnniswitch app
+     */
+    public static final String APP_PACKAGE_NAME = "org.omnirom.omniswitch";
+
+    /**
+     * Intent broadcast action for showing the omniswitch overlay
+     */
+    public static final String ACTION_SHOW_OVERLAY = APP_PACKAGE_NAME + ".ACTION_SHOW_OVERLAY";
+
+    /**
+     * Intent broadcast action for hiding the omniswitch overlay
+     */
+    public static final String ACTION_HIDE_OVERLAY = APP_PACKAGE_NAME + ".ACTION_HIDE_OVERLAY";
+
+    /**
+     * Intent broadcast action for toogle the omniswitch overlay
+     */
+    public static final String ACTION_TOGGLE_OVERLAY = APP_PACKAGE_NAME + ".ACTION_TOGGLE_OVERLAY";
+
+    /**
+     * Intent for launching the omniswitch settings actvity
+     */
+    public static Intent INTENT_LAUNCH_APP = new Intent(Intent.ACTION_MAIN)
+    .setClassName(APP_PACKAGE_NAME, APP_PACKAGE_NAME + ".SettingsActivity");
+
 }
