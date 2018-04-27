bdfb9181450dd982c94a1237cf1d53f72a4c8f0b
martinusbe <martinusbe@gmail.com>
2018-04-27 17:08:14 +0200

    services: Android.mk: little fix for java libraries

--- services/Android.mk
+++ services/Android.mk
@@ -39,15 +39,14 @@ services := \
     usb \
     voiceinteraction
 
-LOCAL_JAVA_LIBRARIES := org.slim.framework
-
 # The convention is to name each service module 'services.$(module_name)'
 LOCAL_STATIC_JAVA_LIBRARIES := $(addprefix services.,$(services)) \
     android.hidl.base-V1.0-java-static \
     android.hardware.biometrics.fingerprint-V2.1-java-static
 
 LOCAL_JAVA_LIBRARIES := \
-    android.hidl.manager-V1.0-java
+    android.hidl.manager-V1.0-java \
+    org.slim.framework
 
 ifeq ($(EMMA_INSTRUMENT_FRAMEWORK),true)
 LOCAL_EMMA_INSTRUMENT := true
