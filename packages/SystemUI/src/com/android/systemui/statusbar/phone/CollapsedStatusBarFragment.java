xWolvesDenx/frameworks_base

Logo: Color picker [1/2]
Adapted to Oreo from our Nougat Tipsy commit

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -47,6 +47,8 @@ import android.widget.ImageView;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuff.Mode;
 
 /**
  * Contains the collapsed status bar and handles hiding/showing based on disable flags
@@ -72,6 +74,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private ImageView mValidusLogo;
     private int mLogoStyle;
     private boolean mShowLogo;
+    private int mLogoColor;
     private final Handler mHandler = new Handler();
 
     private class ValidusSettingsObserver extends ContentObserver {
@@ -85,6 +88,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                     false, this, UserHandle.USER_ALL);
             getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_LOGO_STYLE),
+                    false, this, UserHandle.USER_ALL);
+            getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR),
                     false, this, UserHandle.USER_ALL);
         }
 
@@ -334,6 +340,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mShowLogo = Settings.System.getIntForUser(
                 getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO, 0,
                 UserHandle.USER_CURRENT) == 1;
+        mLogoColor = Settings.System.getIntForUser(
+                getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO_COLOR, 0xff009688,
+                UserHandle.USER_CURRENT);
         mLogoStyle = Settings.System.getIntForUser(
                 getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO_STYLE, 0,
                 UserHandle.USER_CURRENT);
@@ -378,6 +387,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             }
 
             mValidusLogo.setImageDrawable(logo);
+            mValidusLogo.setColorFilter(mLogoColor, PorterDuff.Mode.MULTIPLY);
         }
 
         if (mNotificationIconAreaInner != null) {

anisnoorudheen/frameworks_base-1

packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -50,6 +50,8 @@ import android.widget.ImageView;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuff.Mode;
 
 /**
  * Contains the collapsed status bar and handles hiding/showing based on disable flags
@@ -74,6 +76,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private ImageView mTipsyLogo;
     private int mLogoStyle;
     private boolean mShowLogo;
+    private int mLogoColor;
     private final Handler mHandler = new Handler();
 
     private class TipsySettingsObserver extends ContentObserver {
@@ -87,6 +90,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                     false, this, UserHandle.USER_ALL);
             getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_LOGO_STYLE),
+                    false, this, UserHandle.USER_ALL);
+            getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_LOGO_COLOR),
                     false, this, UserHandle.USER_ALL);
         }
 
@@ -320,6 +326,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mShowLogo = Settings.System.getIntForUser(
                 getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO, 0,
                 UserHandle.USER_CURRENT) == 1;
+        mLogoColor = Settings.System.getIntForUser(
+                getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO_COLOR, 0xff009688,
+                UserHandle.USER_CURRENT);
         mLogoStyle = Settings.System.getIntForUser(
                 getContext().getContentResolver(), Settings.System.STATUS_BAR_LOGO_STYLE, 0,
                 UserHandle.USER_CURRENT);
@@ -364,6 +373,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             }
 
             mTipsyLogo.setImageDrawable(logo);
+            mTipsyLogo.setColorFilter(mLogoColor, PorterDuff.Mode.MULTIPLY);
         }
 
         if (mNotificationIconAreaInner != null) {